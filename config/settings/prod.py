5a353d0f1e74df18da194b498f65ca8479fc01cb
ALLEN KURIAN THOMAS <95510278+Allen-Kurian-Thomas@users.noreply.github.com>
2026-05-19 15:48:37 +0530

    Add Cloudinary configuration to production settings

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -160,3 +160,14 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # UPI Payment Settings
 UPI_ID = os.environ.get('UPI_ID')
 UPI_NAME = os.environ.get('UPI_NAME')
+
+#CLOUDINARY 
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
+
+cloudinary.config(
+    cloud_name=os.getenv('CLOUDINARY_CLOUD_NAME'),
+    api_key=os.getenv('CLOUDINARY_API_KEY'),
+    api_secret=os.getenv('CLOUDINARY_API_SECRET')
+)
